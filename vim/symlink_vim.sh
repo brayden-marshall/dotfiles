ef98e1e05514f3fda548837a8a27e89416528ce7
BraydenMarshall <bmarsh579@gmail.com>
2019-03-26 13:24:09 -0700

    Symlink is working properly

--- vim/symlink_vim.sh
+++ vim/symlink_vim.sh
@@ -1,15 +1,5 @@
-# creating a backup of existing vimrc
-if [ -e ~/.vimrc ]
-then
-    cp --backup=numbered ~/.vimrc ~/.vimrc.bak
-fi
 # link vimrc
-ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf --backup=numbered ~/dotfiles/vim/vimrc ~/.vimrc
 
-# creating a backup of existing molokai colorscheme file
-if [ -e ~/.vim/colors/molokai.vim ]
-then
-    cp --backup=numbered ~/.vim/colors/molokai.vim ~/.vim/colors/molokai.vim.bak
-fi
 # link molokai colorscheme file
-ln -sf ~/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
+ln -sf --backup=numbered ~/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
