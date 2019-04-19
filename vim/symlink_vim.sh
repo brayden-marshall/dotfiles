dca1a30f230e8a0caa9954dfa03212fdab5f1fdf
BraydenMarshall <bmarsh579@gmail.com>
2019-04-19 16:02:01 -0700

    Fixed symlink_vim.sh to check if directory exists

--- vim/symlink_vim.sh
+++ vim/symlink_vim.sh
@@ -2,4 +2,8 @@
 ln -s --backup=numbered ~/dotfiles/vim/vimrc ~/.vimrc
 
 # link molokai colorscheme file
+if [ ! -d "~/.vim/colors" ]
+then
+    mkdir -p ~/.vim/colors
+fi
 ln -s --backup=numbered ~/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
