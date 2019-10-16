89fee0b009cecb94150f06b7a9dca8e76057e471
BraydenMarshall <bmarsh579@gmail.com>
2019-10-16 08:53:17 -0700

    vimrc: added changing airline theme based on colorscheme

--- symlink.sh
+++ symlink.sh
@@ -9,5 +9,8 @@ ln -s --backup=numbered ~/dotfiles/profile ~/.profile
 # link zshrc
 ln -s --backup=numbered ~/dotfiles/zshrc ~/.zshrc
 
+# link tmux.conf
+ln -s --backup=numbered ~/dotfiles/tmux.conf ~/.tmux.conf
+
 # link vim colors directory
 ln -sf ~/dotfiles/vim/colors ~/.vim/colors
