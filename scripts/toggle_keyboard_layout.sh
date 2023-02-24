513dd2ccc841f068e517f80a815895783c72b6d9
Brayden Marshall <bmarsh579@gmail.com>
2023-02-23 21:22:53 -0800

    Random things that need to be pushed.

--- scripts/toggle_keyboard_layout.sh
+++ scripts/toggle_keyboard_layout.sh
@@ -4,6 +4,8 @@ layout=`$DOTFILES/scripts/get_keyboard_layout.sh`
 echo $layout
 if [[ $layout == 'dv' ]]; then
     setxkbmap us
+elif [[ $layout == 'us' ]]; then
+    setxkbmap ru
 else
     setxkbmap dvorak
 fi
